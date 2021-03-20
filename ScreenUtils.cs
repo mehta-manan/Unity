14f26845a4069e4e318a19cbacf8d963a2aabdb5
Manan <mehta.manan@aol.com>
2021-03-20 19:21:44 +0530

    update

--- ScreenUtils.cs
+++ ScreenUtils.cs
@@ -131,8 +131,8 @@ public class ScreenUtils : MonoBehaviour
         rightBottomX = upperRightCornerW.x;
         rightBottomY = lowerLeftCornerW.y;
 
-        centerX = (RightBottomX - LeftBottomX)/2;
-        centerY = (RightBottomY - LeftBottomY)/2;
+        centerX = (RightBottomX - LeftBottomX) / 2;
+        centerY = (RightBottomY - LeftBottomY) / 2;
 
         center = new Vector2(centerX, centerY);
     }
